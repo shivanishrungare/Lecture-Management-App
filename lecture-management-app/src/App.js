6821a7fcd53be211dc1950f0d3be9d74e4b7f14c
Shivani <shivanishrungare@gmail.com>
2024-06-01 23:22:20 +0200

    React app created with initial setup, tailwind css and vercel app setup done

---
+++ lecture-management-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+       <h1 className='text-7xl text-center text-blue-400'>
+          Hello world
+       </h1>
+ 
+    </div>
+  )
+}
+
+export default App
+
