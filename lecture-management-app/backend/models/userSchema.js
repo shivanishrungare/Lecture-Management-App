8eab0f1ded19552e9814135d7b23d4b7d68c3940
Shivani <shivanishrungare@gmail.com>
2024-07-12 23:24:45 +0200

    Updated backend folder and userschema

--- lecture-management-app/backend/models/userSchema.js
+++ lecture-management-app/backend/models/userSchema.js
@@ -38,6 +38,11 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    status: {
+        type: String,
+        enum: ['pending', 'approved', 'rejected'],
+        default: 'pending',
+    },
 },{
     timestamps: true
 })
