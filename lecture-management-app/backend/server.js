203f4f03632e6c4bdab969944c9abb7f24ac99eb
Shivani <shivanishrungare@gmail.com>
2024-09-05 12:13:46 +0200

    server file updated

--- lecture-management-app/backend/server.js
+++ lecture-management-app/backend/server.js
@@ -14,11 +14,13 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin: ["https://lecture-management-app-frtnd.vercel.app"],
-  methods: ["POST", "GET"],
-  credentials: true
-}))
+app.use(cors())
+//   {
+//   origin: ["https://lecture-management-app-frtnd.vercel.app"],
+//   methods: ["POST", "GET"],
+//   credentials: true
+// }
+
 
 app.use(bodyParser.json());
 
