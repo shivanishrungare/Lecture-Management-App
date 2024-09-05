762deed813d856c866d24ced7d0c81d9d76722fe
Shivani <shivanishrungare@gmail.com>
2024-09-05 16:52:47 +0200

    added export app

--- lecture-management-app/backend/server.js
+++ lecture-management-app/backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: "https://lecture-management-app-frtnd.vercel.app",  
+  origin: "https://lecture-management-app-frtnd.vercel.app",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization'],
@@ -37,12 +37,11 @@ mongoose
   .connect(mongodb_uri)
   .then(() => {
     console.log("Connected to MongoDB");
-    const port = process.env.PORT || 5000;
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
-    });
   })
   .catch((error) => {
     console.error("Failed to connect to MongoDB:", error);
     process.exit(1);
   });
+
+
+module.exports = app;
